9be55d2170325a655f22a50a080cf07cc3f50615
4RnoLD31 <116956555+4RnoLD31@users.noreply.github.com>
2022-10-31 18:52:17 +0300

    Update settings.js

--- javascript/settings.js
+++ javascript/settings.js
@@ -1,5 +1,5 @@
 const searchString = new URLSearchParams(window.location.search);
 const id = searchString.get('modelClientId');
 if (id == "2717501C_1000") {
-  document.write("{"code":10000,"status":0,"result":[]}");
+  document.write({"code":10000,"status":0,"result":[]})
 }
