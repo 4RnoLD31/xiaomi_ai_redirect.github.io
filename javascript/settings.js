cc4139c7c4ac1632412d5d46b8ff8a7720904920
4RnoLD31 <116956555+4RnoLD31@users.noreply.github.com>
2022-10-31 18:49:14 +0300

    Update settings.js

--- javascript/settings.js
+++ javascript/settings.js
@@ -1,10 +1,5 @@
 const searchString = new URLSearchParams(window.location.search);
-const clickid = searchString.get('clickid');
-console.log(clickid);
-document.write(clickid);
-if (clickid == 45) {
-  document.write( "Правильно!" );
-  document.write( "Вы такой умный!" );
-} if (clickid == 23) {
-  document.write( "Правильно!" )
+const id = searchString.get('modelClientId');
+if (id == "2717501C_1000") {
+  document.write("{"code":10000,"status":0,"result":[]}");
 }
