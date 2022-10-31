6c83533dc744da53546325a889ac81b862b426ae
4RnoLD31 <116956555+4RnoLD31@users.noreply.github.com>
2022-10-31 18:03:19 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
 const searchString = new URLSearchParams(window.location.search);
 const clickid = searchString.get('clickid');
 console.log(clickid);
-document.write(clickid)
+document.write(clickid);
+if (clickid == 45) {
+  document.write( "Правильно!" );
+  document.write( "Вы такой умный!" );
+} if (clickid == 23) {
+  document.write( "Правильно!" )
+}
